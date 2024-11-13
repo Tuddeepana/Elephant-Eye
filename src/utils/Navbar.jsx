e8e40be4765349b035d9c1b6a98520414fb97a1d
DhananjayaDilz <dhananjaya.20221596@iit.ac.lk>
2024-11-13 21:57:08 +0530

    update nav bar

--- src/utils/Navbar.jsx
+++ src/utils/Navbar.jsx
@@ -25,7 +25,7 @@ const Navbar = () => {
   return (
     <nav
       className={`fixed top-0 left-0 w-full z-50 p-1 transition-colors duration-300 ${
-        isScrolled ? "bg-white bg-opacity-80 text-black" : "bg-black bg-opacity-70 text-white"
+        isScrolled ? "bg-white  text-black" : "bg-black bg-opacity-70 text-white"
       }`}
     >
       <div className="max-w-7xl mx-auto flex justify-between items-center">
@@ -40,8 +40,8 @@ const Navbar = () => {
 
         {/* Left Links Section */}
         <div className={`hidden lg:flex space-x-6`}>
-          <a href="#rooms" className="hover:text-gray-200">Rooms</a>
-          <a href="#offers" className="hover:text-gray-200">Offers</a>
+          <a href="#rooms" className="hover:text-yellow-500">Rooms</a>
+          <a href="#offers" className="hover:text-yellow-500">Offers</a>
         </div>
 
         {/* Centered Logo */}
@@ -51,8 +51,8 @@ const Navbar = () => {
 
         {/* Right Links Section */}
         <div className={`hidden lg:flex space-x-6 items-center`}>
-          <a href="#experiences" className="hover:text-gray-200">Experiences</a>
-          <a href="#contact" className="hover:text-gray-200">Contact Us</a>
+          <a href="#experiences" className="hover:text-yellow-500">Experiences</a>
+          <a href="#contact" className="hover:text-yellow-500">Contact Us</a>
           <a
             href="#book"
             className="px-4 py-2 bg-yellow-700 text-white font-semibold rounded hover:bg-yellow-800"
